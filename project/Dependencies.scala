706db86047a80a5115ff392291f9c9693ba610d2
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-18 12:56:17 +0000

    Reformat with scalafmt 3.7.14
    Executed command: scalafmt --non-interactive

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
   lazy val sttpCore = "com.softwaremill.sttp.client3" %% "core" % sttpVersion
   lazy val sttpFs2 = "com.softwaremill.sttp.client3" %% "fs2" % sttpVersion
-  lazy val log4Cats =  "org.typelevel" %% "log4cats-slf4j"   % "2.6.0"
+  lazy val log4Cats = "org.typelevel" %% "log4cats-slf4j" % "2.6.0"
   lazy val sttpUpickle = "com.softwaremill.sttp.client3" %% "upickle" % sttpVersion
   lazy val sttpZio = "com.softwaremill.sttp.client3" %% "zio" % sttpVersion
   lazy val awsSecretsManager = "software.amazon.awssdk" % "secretsmanager" % "2.20.26"
